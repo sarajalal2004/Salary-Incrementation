58e112293b0ddc39196cae131d6b154157ff9724
sarajalal2004 <sarauobeducation@gmail.com>
2026-02-16 20:10:56 +0300

    Controller main class

--- src/main/java/com/ga/employeeSalary/controllers/EmployeeSalaryController.java
+++ src/main/java/com/ga/employeeSalary/controllers/EmployeeSalaryController.java
@@ -1,5 +1,22 @@
 package com.ga.employeeSalary.controllers;
 
-public class EmployeeSalaryController {
+import com.ga.employeeSalary.services.EmployeeSalaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+@RestController
+@RequestMapping(path = "/api/salary")
+public class EmployeeSalaryController {
+    private EmployeeSalaryService employeeSalaryService;
+
+    @Autowired
+    private void setEmployeeSalaryService(EmployeeSalaryService employeeSalaryService){
+        this.employeeSalaryService = employeeSalaryService;
+    }
 }
