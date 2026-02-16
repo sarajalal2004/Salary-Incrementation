8c699006e6051282a1e1316a3c435eb148a3a395
sarajalal2004 <sarauobeducation@gmail.com>
2026-02-16 20:04:17 +0300

    define main attribute

--- src/main/java/com/ga/employeeSalary/models/Employee.java
+++ src/main/java/com/ga/employeeSalary/models/Employee.java
@@ -1,5 +1,24 @@
 package com.ga.employeeSalary.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Employee {
+    private Integer id;
+    private String name;
+    private Double salary;
+    private LocalDate joinDate;
+    private String role;
+    private Double projectProgress;
+    
 
 }
