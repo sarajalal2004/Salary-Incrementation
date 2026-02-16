aac5a7122fbc7488f875e3fd5b7b408ca74f05e0
sarajalal2004 <sarauobeducation@gmail.com>
2026-02-16 20:09:07 +0300

    CSV file read

--- src/main/java/com/ga/employeeSalary/services/EmployeeSalaryService.java
+++ src/main/java/com/ga/employeeSalary/services/EmployeeSalaryService.java
@@ -1,5 +1,25 @@
 package com.ga.employeeSalary.services;
 
-public class EmployeeSalaryService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
+@Service
+public class EmployeeSalaryService {
+    private CSVProcessor csvProcessor;
+
+    @Autowired
+    private void setCSVProcessor(CSVProcessor csvProcessor){
+        this.csvProcessor = csvProcessor;
+    }
+
+    public void incrementSalaries(MultipartFile file){
+        File outputfile = csvProcessor.UpdateSalaries(file,"Updated" + file.getOriginalFilename().substring(0,1).toUpperCase() + file.getOriginalFilename().substring(1));
+    }
 }
